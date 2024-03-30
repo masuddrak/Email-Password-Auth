1be03240f6649d71a23a3252a98d0c1eb5b82fa4
masud <masudranaacb@gmail.com>
2024-03-31 01:59:32 +0600

    added password validatin regular expression

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,22 +2,33 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { app } from "../firebase/friebase.config";
 import { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { IoMdEyeOff } from "react-icons/io";
 
 
 
 const Login = () => {
     const [errorMessage, setErrorMessage] = useState('')
-    const [success,setSuccess]=useState("")
+    const [success, setSuccess] = useState("")
+    const [seePassword, setSeePassword] = useState(false)
+    console.log(seePassword)
     const auth = getAuth(app);
+    const myCheckPassword = /^(?=.*[A-Z])(?=.*[@$!%*?&])[a-zA-Z@$!%*?&]/
+
     const handelSubmit = (e) => {
         e.preventDefault()
         const email = e.target.email.value
         const password = e.target.password.value
         // clint site password validatin
-        if(password.length<6){
+        if (password.length < 6) {
             setErrorMessage("please 6 loger password type now")
             return
         }
+        else if (!myCheckPassword.test(password)) {
+            setErrorMessage("plase type 1 UperCase")
+            return
+        }
+
         // set rest erro
         setErrorMessage("")
         setSuccess("")
@@ -51,13 +62,20 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input name="password" type="password" placeholder="password" className="input input-bordered" required />
+                                <div className=" relative">
+                                    <input name="password" type={seePassword ? "text" : "password"} placeholder="password" className="input input-bordered w-full" required />
+                                    <div className="absolute right-2 top-2 ">
+                                        {
+                                            seePassword ? <h1 onClick={() => setSeePassword(!seePassword)}><FaEye></FaEye></h1> : <h1 onClick={() => setSeePassword(!seePassword)}><IoMdEyeOff></IoMdEyeOff></h1>
+                                        }
+                                    </div>
+                                </div>
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <input className="bg-green-500 text-white p-3" type="submit" value="Register"></input>
                             </div>
                             {
                                 errorMessage && <p className="text-red-500">{errorMessage}</p>
